9208bc8d0143825f5a423e30d265e2b12a242d73
salwa <salwa.boukhrissi-semmane@capgemini.com>
2023-06-05 18:11:47 +0200

    Clase idioma creada

--- src/main/java/com/example/entities/Idioma.java
+++ src/main/java/com/example/entities/Idioma.java
@@ -2,11 +2,13 @@ package com.example.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,17 +25,16 @@ public class Idioma implements Serializable{
 
     private static final Long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
     private String idioma;
 
-
+    @OneToMany(fetch = FetchType.LAZY, cascade =CascadeType.PERSIST, mappedBy = "idiomas")
     private Nivel nivel;
 
-    
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy = "idiomas")
     private Asistente asistente;
 
     
