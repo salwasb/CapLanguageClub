46c994cb35a8804b34409feef690ecf3c95cfa69
nadia <quimica-2009@hotmail.es>
2023-06-05 17:41:01 +0200

    entity asistente rellenada

--- src/main/java/com/example/entities/Asistente.java
+++ src/main/java/com/example/entities/Asistente.java
@@ -1,5 +1,32 @@
 package com.example.entities;
 
-public class Asistente {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "asistentes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Asistente implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String correo;
     
 }
