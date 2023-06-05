e964a594586b7a3c0049f4e14890175f78cff6b2
pilarsm <pilar.sm02@gmail.com>
2023-06-05 18:01:37 +0200

    clase nivel creada

--- src/main/java/com/example/entities/Nivel.java
+++ src/main/java/com/example/entities/Nivel.java
@@ -1,5 +1,39 @@
 package com.example.entities;
 
-public class Nivel {
+import java.io.Serializable;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "niveles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Nivel implements Serializable {
+
+    private static final long  serialVersionUID =1L;
     
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String nivel;
+
+    
+    
+
+
 }
