a16774049d8ece6cf5f3a6956cee0af59cbd6ce3
yakouta <yakouta.a@hotmail.es>
2023-06-05 18:01:54 +0200

    creada la clase

--- src/main/java/com/example/entities/Idioma.java
+++ src/main/java/com/example/entities/Idioma.java
@@ -1,5 +1,40 @@
 package com.example.entities;
 
-public class Idioma {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table( name = "idiomas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Idioma implements Serializable{
+
+    private static final Long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String idioma;
+
+
+    private Nivel nivel;
+
+    
+    private Asistente asistente;
+
     
 }
