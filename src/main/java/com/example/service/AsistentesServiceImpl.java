45f6222661661abba96e4189ae0071b819e8dd34
nadia <quimica-2009@hotmail.es>
2023-06-06 18:47:39 +0200

    Asistente Dao y Service

---
+++ src/main/java/com/example/service/AsistentesServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dao.AsistenteDao;
+import com.example.entities.Asistente;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AsistentesServiceImpl implements AsistentesService {
+
+    private final AsistenteDao asistenteDao;
+
+    @Override
+    public List<Asistente> findAll() {
+       return asistenteDao.findAll();
+    }
+
+    @Override
+    public Asistente findById(int idAsistente) {
+      return asistenteDao.findById(idAsistente).get();
+    }
+
+    @Override
+    @Transactional
+    public void saveAsistente(Asistente asistente) {
+       asistenteDao.save(asistente);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAsistente(Asistente asistente) {
+      asistenteDao.delete(asistente);
+    }
+    
+}
