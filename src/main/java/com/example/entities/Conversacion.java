dc18e7bb69a8d3548332bc7c80daec9ea3ade95c
rachida <rachida.ragmi-houari@capgemini.com>
2023-06-06 23:03:44 +0200

    Conversacion Service

--- src/main/java/com/example/entities/Conversacion.java
+++ src/main/java/com/example/entities/Conversacion.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,6 +18,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,13 +37,23 @@ import lombok.NoArgsConstructor;
 public class Conversacion implements Serializable{
 
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; 
 
+    @NotNull(message = "Le nom ne peut pas être null")
+    @NotBlank(message = "Le nom ne peut pas être vide")
     private String titulo; 
+
+    @NotNull(message = "Le lieu ne peut pas être null")
+    @NotBlank(message = "Le lieu ne peut pas être vide")
     private String lugar;
-    private int numeroMaxAsistentes; 
+
+    @Size(min = 4, message = "Le nombre de participants ne peut être moins que quatre et plus que huit")
+    private int numeroAsistentes; 
+
+    
     private LocalDate fecha;
     private LocalTime hora; 
 
@@ -51,5 +67,6 @@ public class Conversacion implements Serializable{
     private Nivel nivel; 
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy = "conversacion")
+    @JsonIgnore
     private List<Asistente> asistentes;
 }
