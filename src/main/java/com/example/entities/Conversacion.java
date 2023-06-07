02a758cbf0523712909f562cb8bad6456bf239e1
rachida <rachida.ragmi-houari@capgemini.com>
2023-06-07 12:34:43 +0200

    Nada

--- src/main/java/com/example/entities/Conversacion.java
+++ src/main/java/com/example/entities/Conversacion.java
@@ -18,7 +18,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
