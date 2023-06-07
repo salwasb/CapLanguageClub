d07ae7f306f36f6e2f31f0b81326ba66807bc756
salwa <salwa.boukhrissi-semmane@capgemini.com>
2023-06-07 09:24:05 +0200

    Conversacion Actualizada

--- src/main/java/com/example/dao/ConversacionDao.java
+++ src/main/java/com/example/dao/ConversacionDao.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entities.Conversacion;
 
-<<<<<<< HEAD
 public interface ConversacionDao extends JpaRepository<Conversacion, Integer> {
-=======
-public interface ConversacionDao extends JpaRepository<Conversacion, Integer>{
->>>>>>> t
+
     
     
 }
