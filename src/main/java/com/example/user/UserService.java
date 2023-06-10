7dfec083a8003c693dd57ee474bd4e9d10c28c1a
salwa <salwa.boukhrissi-semmane@capgemini.com>
2023-06-10 11:25:04 +0200

    Seguridad actualizada

--- src/main/java/com/example/user/UserService.java
+++ src/main/java/com/example/user/UserService.java
@@ -3,9 +3,10 @@ package com.example.user;
 import java.util.List;
 
 public interface UserService {
-    User add(User user); // todos los metodos de una inteface por defecto son public y abstract.
-    List<User> findAll();
-    void deleteByEmail(String email);
-    User findByEmail(String email);
-    User update(User user);    
+    
+    public abstract User add(User user); // todos los metodos de una inteface por defecto son public y abstract.
+    public abstract List<User> findAll();
+    public abstract void deleteByEmail(String email);
+    public abstract User findByEmail(String email);
+    public abstract User update(User user);
 }
