209189e161f22ab00575ab6cd8466d9ebee6dd13
nadia <quimica-2009@hotmail.es>
2023-06-10 18:14:45 +0200

    upload file y security

---
+++ src/main/java/com/example/user/UserService.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import java.util.List;
+
+public interface UserService {
+    
+    public abstract User add(User user); // todos los metodos de una inteface por defecto son public y abstract.
+    public abstract List<User> findAll();
+    public abstract void deleteByEmail(String email);
+    public abstract User findByEmail(String email);
+    public abstract User update(User user);
+}
+
